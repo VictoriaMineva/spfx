90e62c99ec21c3a46b537092d8f99bb55ed91661
VictoriaMineva <victoriamineva@live.com>
2020-01-13 07:05:10 -0800

    Day One

---
+++ Labs/FirstOfMany/src/webparts/userInformation/UserInformationWebPart.ts
@@ -0,0 +1,57 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './UserInformationWebPart.module.scss';
+import * as strings from 'UserInformationWebPartStrings';
+
+export interface IUserInformationWebPartProps {
+  greeting: string;
+}
+
+export default class UserInformationWebPart extends BaseClientSideWebPart <IUserInformationWebPartProps> {
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${ styles.userInformation }">
+    <div class="${ styles.container }">
+      <div class="${ styles.row }">
+        <div class="${ styles.column }">
+          ${this.properties.greeting},&nbsp;
+          ${this.context.pageContext.user.displayName}
+          </div>
+          </div>
+          </div>
+          </div>`;
+  }
+
+  protected get dataVersion(): Version {
+  return Version.parse('1.0');
+}
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+  return {
+    pages: [
+      {
+        header: {
+          description: strings.PropertyPaneDescription
+        },
+        groups: [
+          {
+            groupName: strings.BasicGroupName,
+            groupFields: [
+              PropertyPaneTextField('greeting', {
+                label: strings.GreetingFieldLabel
+              })
+            ]
+          }
+        ]
+      }
+    ]
+  };
+}
+}
